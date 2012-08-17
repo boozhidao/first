585acd275d9a2f419921fa1bf95015262a062cb0
boozhidao <boozhidao@gmail.com>
2012-08-16 23:18:13 -0600

    first around

---
+++ yiwubeth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yiwubeth::Application.config.secret_token = 'fb7a7a2827da21d524cb644debf8ba8a8711349cb27cb25661c76eb9c834bbcdf87c621c3c6a0c196a8ac481aea3f82c1e18a05cb84d1d795014ac0edf015e0b'
---
+++ yiwubeth/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yiwubeth::Application.config.session_store :cookie_store, key: '_yiwubeth_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yiwubeth::Application.config.session_store :active_record_store
